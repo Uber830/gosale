76eed4b3bd5c8918a64fcd8e32daa2d9a77fca66
satup <gamboaale@misena.edu.co>
2021-09-01 19:18:53 -0500

    se agregan activitys de visitas

---
+++ app/src/main/java/com/sena/gosale/Activity_visita1.kt
@@ -0,0 +1,11 @@
+package com.sena.gosale
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_visita1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_visita1)
+    }
+}
