9a2ed7f35213c00913080b5a51519b0882cfcd20
Uber830 <hhcardoso@misena.edu.co>
2021-09-01 20:46:56 -0500

    v4

--- app/src/main/java/com/sena/gosale/ClientesLocalizacionActivity.kt
+++ app/src/main/java/com/sena/gosale/ClientesLocalizacionActivity.kt
@@ -2,10 +2,41 @@ package com.sena.gosale
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class ClientesLocalizacionActivity : AppCompatActivity() {
+class ClientesLocalizacionActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var map:GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_clientes_localizacion)
+
+        createFragment()
+    }
+
+    private fun createFragment() {
+        val mapFragment : SupportMapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this) }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        createMarker()
+    }
+
+    private fun createMarker() {
+        val coordinates = LatLng(4.7063778,-74.2256662)
+        val marker : MarkerOptions = MarkerOptions().position(coordinates).title("Parque Principal Mosquera")
+        map.addMarker(marker)
+        map.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(coordinates, 18f),
+            4000,
+            null
+        )
     }
 }
